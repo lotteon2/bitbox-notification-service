2458637cec6d775002e40dfbe1265b07ad65b739
wakkpu <ho0524870@gmail.com>
2023-10-25 16:29:00 +0900

    [FEAT] send sse

--- src/main/java/com/bitbox/notification/service/SseService.java
+++ src/main/java/com/bitbox/notification/service/SseService.java
@@ -1,8 +1,15 @@
 package com.bitbox.notification.service;
 
+import com.bitbox.notification.dto.NotificationTuple;
+import com.bitbox.notification.entity.Notification;
 import com.bitbox.notification.repository.EmitterRepository;
+import com.bitbox.notification.repository.NotificationRepository;
+import io.github.bitbox.bitbox.dto.NotificationDto;
+import io.github.bitbox.bitbox.enums.NotificationType;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
@@ -12,10 +19,11 @@ import java.io.IOException;
 public class SseService {
 
     private final EmitterRepository emitterRepository;
+    private final NotificationRepository notificationRepository;
 
     private final long TIME_OUT = 60L * 60L * 1000L;
 
-    private final String WELCOME_MESSAGE = "WELCOME TO BITBOX";
+    private final String WELCOME_MESSAGE = "Hello, BITBOX";
 
     public SseEmitter subscribe(String memberId) {
         String emitterId = makeTimeIncludeId(memberId);
@@ -35,11 +43,36 @@ public class SseService {
         return memberId + "_" + System.currentTimeMillis();
     }
 
-    private void sendNotification(SseEmitter sseEmitter, String eventId, String emitterId, Object notification) {
+    private void sendNotification(SseEmitter sseEmitter, String eventId, String emitterId, Object notificationInfo) {
         try {
-            sseEmitter.send(SseEmitter.event().id(eventId).data(notification));
+            sseEmitter.send(SseEmitter.event().id(eventId).data(notificationInfo));
         } catch (IOException e) {
             emitterRepository.deleteByEmitterId(emitterId);
         }
+    }
+
+    @KafkaListener(topics = "alarmTopic")
+    public void send(NotificationDto notificationDto) { // alarmTopic Consume해서
+
+        // NotificationType 따라 url과 info 달라짐.
+        NotificationTuple notificationTuple = NotificationTuple.strategies(notificationDto);
+
+        // 해당 사용자의 emitter들에 모두 send
+        emitterRepository.findAllEmitterStartWithMemberId(notificationDto.getReceiverId()).forEach(
+                (emitterId, sseEmitter) -> sendNotification(
+                        sseEmitter,
+                        makeTimeIncludeId(notificationDto.getReceiverId()),
+                        emitterId,
+                        notificationTuple.getNotificationInfo()
+                )
+        );
+
+        // db에 알림 저장
+        notificationRepository.save(Notification.builder()
+                .memberId(notificationDto.getReceiverId())
+                .notificationLink(notificationTuple.getNotificationLink())
+                .notificationInfo(notificationTuple.getNotificationInfo())
+                .build()
+        );
     }
 }
